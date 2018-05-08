b03ff8355eaf57732fd8a7e021e9dc0cdfd4f8a7
Alejandro Lozano <alejandro.lozano@nxp.com>
2018-05-08 09:12:09 -0500

    2 Triangles one VAO each

--- ogl1/main2T2VAO.cpp
+++ ogl1/main2T2VAO.cpp
@@ -1,4 +1,3 @@
-#if 0
 #include <iostream>
 #include <fstream>
 //#define GLEW_STATIC
@@ -54,34 +53,36 @@ int main()
 	myShader.CreateProgram("Vertex.hlsl", "Frag.hlsl");
 
 	//My triangle vertices
-	GLfloat vertices[] =
+	GLfloat vertices1[] =
 	{
 		/*Square*/
 		-0.25f, -0.25f, 0.0f,
 		0.25f, -0.25f, 0.0f,
 		0.25f, 0.25f, 0.0f,
 
-		-0.25f, -0.25f, 0.0f,
-		0.25f, 0.25f, 0.0f,
-		-0.25f, 0.25f, 0.0f
 	};
 
+	//My triangle vertices
+	GLfloat vertices2[] =
+	{
+		/*Square*/
+		-0.1f, -0.1f, 0.0f,
+		0.1f, 0.1f, 0.0f,
+		-0.1f, 0.1f, 0.0f
+	};
 
-	GLuint VBO, VAO;
+	GLuint VBO[2], VAO[2];
 	//Create Vertex Array Object to store attributespointer and disable/enable attributes and EBO
-	glGenVertexArrays(1, &VAO);
-
+	glGenVertexArrays(2, VAO);
 	//Generate Buffer object, batch of data.. ussually vertices
-	glGenBuffers(1, &VBO);
+	glGenBuffers(2, VBO);
 
 	//Bind Vertex Array Object
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO[0]);
 	//Bind VBO as Buffer Object
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	//Copy Data, in this case vertices to the bound buffer. Data will not change or very rarely
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 	/*Point to  Attrib pointer*/
 	/* Attrib 0,  3 elements, type of element, not normalized,  stride next element after 3 GL_FLOATS, offset of firsrt element */
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
@@ -91,6 +92,24 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	//Unbind Vertex Array
 	glBindVertexArray(0);
+
+
+	//Bind Vertex Array Object
+	glBindVertexArray(VAO[1]);
+	//Bind VBO as Buffer Object
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	//Copy Data, in this case vertices to the bound buffer. Data will not change or very rarely
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	/*Point to  Attrib pointer*/
+	/* Attrib 0,  3 elements, type of element, not normalized,  stride next element after 3 GL_FLOATS, offset of firsrt element */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+	/*Enable Vertex Attribute*/
+	glEnableVertexAttribArray(0);
+	//Unbind VBO. VBO binding is stored in Attributes
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//Unbind Vertex Array
+	glBindVertexArray(0);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -102,8 +121,11 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		/*Use this Shader program*/
 		myShader.UseShader();
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES,0, 6);
+		glBindVertexArray(VAO[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(VAO[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
 
 		/*swap double buffer*/
@@ -118,17 +140,5 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
-
-
-
-#endif
-
-
-
-
-
-
-
-
 
 
